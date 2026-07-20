82ae3788a63ea59b03f972f03822863afad2a6ca
cinarkundakci2011-sudo <cinarkundakci2011@gmail.com>
2026-07-20 17:03:01 +0300

    Create HudConfig.java

---
+++ src/main/java/com/example/perfhud/HudConfig.java
@@ -0,0 +1,10 @@
+package com.example.perfhud;
+
+public class HudConfig {
+    public static boolean enabled = true;
+    public static int x = 10;
+    public static int y = 10;
+    public static int textColor = 0xFFFFFFFF;       // Fully opaque ARGB White
+    public static int backgroundColor = 0x80000000; // Semi-transparent black box
+    public static int highlightColor = 0xFFFF5555;   // Red-orange highlight tint
+}
